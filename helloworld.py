f1ae01ba2561a22c1699cdbd8c2aaf660221ec82
Stephanie Yang <syangvan2967@gmail.com>
2023-09-10 17:40:35 -0400

    commit c2

--- helloworld.py
+++ helloworld.py
@@ -2,11 +2,7 @@
 print('Hello World')
 print('Stephanie')
 print('4 years')
-<<<<<<< HEAD
 print('this is commit c2')
 print('this is commit c3')
 print('this is commit c4')
-=======
 print('this is commit c1')
-
->>>>>>> a9262d2 (commit c1)
